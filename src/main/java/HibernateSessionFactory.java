7ce78e8f27c9c469fa2ff4c2c01e324bcc330410
Anton <2323353@gmail.com>
2019-10-18 20:35:52 +0300

    hibernate comm 2

---
+++ src/main/java/HibernateSessionFactory.java
@@ -0,0 +1,32 @@
+import org.hibernate.HibernateException;
+import org.hibernate.Metamodel;
+import org.hibernate.query.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import javax.persistence.metamodel.EntityType;
+
+import java.util.Map;
+
+public class HibernateSessionFactory {
+
+    private static final SessionFactory ourSessionFactory;
+
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+
+            ourSessionFactory = configuration.buildSessionFactory();
+        } catch (Throwable ex) {
+            ex.printStackTrace();
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static Session getSession() throws HibernateException {
+        return ourSessionFactory.openSession();
+    }
+
+}
